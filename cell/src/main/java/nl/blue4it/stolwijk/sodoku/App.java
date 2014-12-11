88b242220422bf18b960eb00666e5254d89e578c
Nick Stolwijk <nick.stolwijk@gmail.com>
2014-12-11 21:59:51 +0100

    Added initial project structure.

---
+++ cell/src/main/java/nl/blue4it/stolwijk/sodoku/App.java
@@ -0,0 +1,13 @@
+package nl.blue4it.stolwijk.sodoku;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
